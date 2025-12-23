1af5bb9238eb24df788a7dd7d4441170954695b8
ekks09 <kibetk800@gmail.com>
2025-12-23 17:31:40 +0300

    Update urls.py

--- Backend/loans/urls.py
+++ Backend/loans/urls.py
@@ -3,5 +3,5 @@ from .views import ApplyLoanView, CurrentLoanView
 
 urlpatterns = [
     path("apply/", ApplyLoanView.as_view(), name="apply-loan"),
-    path("current/", CurrentLoanView.as_view(), name="current-loan"),
+    path("active/", CurrentLoanView.as_view(), name="current-loan"),  # changed from "current/" to "active/"
 ]
