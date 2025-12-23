3f6468cc8914c39f984c130872cb329a463b1cf8
ekks09 <kibetk800@gmail.com>
2025-12-23 15:46:23 +0300

    Update views.py

--- Backend/users/views.py
+++ Backend/users/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework import status
 from .authentication import _jwt_encode
 from .serializers import RegisterSerializer, LoginSerializer, MeSerializer
 
@@ -23,15 +23,26 @@ class RegisterView(APIView):
         return Response({"ok": True}, status=201)
 
 
+
+
 class LoginView(APIView):
     permission_classes = [permissions.AllowAny]
 
     def post(self, request):
-        s = LoginSerializer(data=request.data)
-        s.is_valid(raise_exception=True)
-        user = s.validated_data["user"]
-        token = _jwt_encode(user)
-        return Response({"access": token})
+        try:
+            s = LoginSerializer(data=request.data)
+            s.is_valid(raise_exception=True)
+            user = s.validated_data["user"]
+            token = _jwt_encode(user)
+            return Response({"access": token})
+        except serializers.ValidationError as e:
+            return Response({"error": e.detail}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
+            return Response(
+                {"error": "An unexpected error occurred."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
+
 
 
 class MeView(APIView):
