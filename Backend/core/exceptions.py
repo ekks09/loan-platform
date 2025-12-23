468439f85e0f62ce78d1c071484db44f79aab2ed
ekks09 <kibetk800@gmail.com>
2025-12-23 17:21:08 +0300

    Update exceptions.py

--- Backend/core/exceptions.py
+++ Backend/core/exceptions.py
@@ -1,5 +1,7 @@
 # core/exceptions.py
 from rest_framework.views import exception_handler
+from rest_framework.response import Response
+
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
