23678ba57c89771a21c578a6b9fedc9d4cc753bd
ekks09 <kibetk800@gmail.com>
2025-12-23 18:35:13 +0300

    Update urls.py

--- Backend/payments/urls.py
+++ Backend/payments/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ConfirmPaymentView
+from .views import InitPaymentView, VerifyPaymentView
 
 urlpatterns = [
     path("init/", InitPaymentView.as_view(), name="payment-init"),
     path("verify/", VerifyPaymentView.as_view(), name="payment-verify"),
-    path("confirm/", ConfirmPaymentView.as_view(), name="confirm-payment"),
 ]
+
+
+
+
+
+
+
